306b6d6e45ad3faf19faa1698be36ce909dbd766
Melrose Ceballos <melroseceballos@Melroses-MBP.lan>
2023-03-07 15:48:01 -0800

    Scaffolded and created&Exported Schema

---
+++ models/fishSchema.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+// CREATING SCHEMA 
+const fishSchema = new mongoose.Schema({
+    Name: {type: String, required: true },
+    Description: {type: String, required: true},
+    Rating: {type: String},
+    Price: {type: Number},
+    Quantity: {type: Number}
+})
+
+// EXPORT THE SCHEMA AS A MONGOOSE MODEL 
+module.exports = mongoose.model('Fish', fishSchema)
---
+++ models/index.js
@@ -0,0 +1,10 @@
+// CONNECTING EXPRESS APP TO MONGO DATABASE
+require('dotenv').config()
+const mongoose = require('mongoose')
+const mongodburi=process.env.MONGODBURI;
+
+// ASYNC FUNCTION THAT WILL CONNECT MONGOOSE TO MONGO DATABASE
+(async function () {
+    await mongoose.connect(mongodburi);
+    console.log("You are now connected to" , mongodburi);})
+().catch(err => console.log('Failed to connect: \n' + err))
